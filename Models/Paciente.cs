5f47fa1ae122905fa69aeaa6f501cfc400373903
Luahr Rocha <73291538+luahr13@users.noreply.github.com>
2025-11-20 17:17:52 -0300

    Paciente UltimoLogin = dateTime.Now

--- Models/Paciente.cs
+++ Models/Paciente.cs
@@ -7,6 +7,6 @@
         public string Email { get; set; }
         public string SenhaHash { get; set; }
         public DateTime CriadoEm { get; set; }
-        public DateTime? UltimoLoginEm { get; set; }
+        public DateTime? UltimoLoginEm { get; set; } = DateTime.Now;
     }
 }
