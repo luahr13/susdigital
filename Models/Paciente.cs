a9cb1eb53620e37e0e4ffcc4d6bf18b4166eaf62
Luahr Rocha <73291538+luahr13@users.noreply.github.com>
2025-11-20 17:05:31 -0300

    Atributos de Paciente

--- Models/Paciente.cs
+++ Models/Paciente.cs
@@ -2,5 +2,12 @@
 {
     public class Paciente
     {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string SenhaHash { get; set; }
+        public DateTime CriadoEm { get; set; }
+        public DateTime? UltimoLoginEm { get; set; }
+        public UserRole Papel { get; set; }
     }
 }
