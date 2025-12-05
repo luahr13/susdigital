483155a31eb4039bb91d4ecbaa4a2052d217471f
Luahr Rocha <73291538+luahr13@users.noreply.github.com>
2025-12-04 23:34:44 -0300

    Enum Sexo, Raça, incremento de paciente, Modelo Alergia e Patologia

---
+++ Migrations/20251205023416_EnumModelPatologias.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace projetoTP3_A2.Migrations
+{
+    /// <inheritdoc />
+    public partial class EnumModelPatologias : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
